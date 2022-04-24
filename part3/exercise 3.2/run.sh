66331675c72b5c81ba09ce7285cfb9da4d831794
Ville Virtanen <cvivi007@edu.xamk.fi>
2022-04-24 20:55:25 +0300

    Exercise 3.2 done

--- part3/exercise 3.2/run.sh
+++ part3/exercise 3.2/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/cvivi007xamk/docker-hy.github.io.git
-docker build -t villeovirtanen/exercise-3.2:latest ./docker-hy.github.io/   
+git clone https://github.com/cvivi007xamk/simple-web-service.git
+docker build -t villeovirtanen/exercise-3.2 ./simple-web-service/   
 docker login
-docker push villeovirtanen/exercise-3.2:latest
+docker push villeovirtanen/exercise-3.2
